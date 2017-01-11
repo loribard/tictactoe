9181e257ddf4cc89d62514b4e8cfe21367bd855c
Lori Bard <lori.r.bard@gmail.com>
2017-01-10 18:53:34 -0800

    playing with Mapbox

--- tictactoe.py
+++ tictactoe.py
@@ -1,24 +1,30 @@
+"""Tic Tac Toe is a game for two players. If all X's or all O's form a 
+vertical,horizontal, or diagonal line, then the player with the X's or
+O's wins the game. 
+"""
+
+
 def gameboard(list_x_o=None, win=None):
     """ This function prints out the gameboard."""
     if list_x_o is None:
-        list_x_o = [["","",""],["","",""],["","",""]]
+        list_x_o = [["", "", ""], ["", "", ""], ["", "", ""]]
     vertical_divide = "               |          |               "
-    horizontal_divide = "       _____________________________"      
+    horizontal_divide = "       _____________________________"    
     print '/n' * 5
     print "Column    0          1          2          "
     print vertical_divide
-    print "Row 0     %s     |     %s     |     %s          " % (list_x_o[0][0],list_x_o[0][1],list_x_o[0][2])
+    print "Row 0     %s     |     %s     |     %s          " % (list_x_o[0][0], list_x_o[0][1], list_x_o[0][2])
     print vertical_divide
     print horizontal_divide
     print vertical_divide                      
-    print "Row 1     %s     |     %s     |     %s          " % (list_x_o[1][0],list_x_o[1][1],list_x_o[1][2])
+    print "Row 1     %s     |     %s     |     %s          " % (list_x_o[1][0], list_x_o[1][1], list_x_o[1][2])
     print vertical_divide
     print horizontal_divide
     print vertical_divide
-    print "Row 2     %s     |     %s     |     %s         " % (list_x_o[2][0],list_x_o[2][1],list_x_o[2][2])  
+    print "Row 2     %s     |     %s     |     %s         " % (list_x_o[2][0], list_x_o[2][1], list_x_o[2][2])  
     print vertical_divide  
 
-    if win == True or win == False:
+    if win is True or win is False:
         start()
     else:
         new_value(list_x_o)
@@ -27,7 +33,7 @@ def gameboard(list_x_o=None, win=None):
 def new_value(list_x_o):
     """ This function asks the player which number they are and where they want to put their mark."""
     player_num = raw_input("Are you Player #1 or Player #2? (1/2) ")
-    if player_num not in ['1','2']:
+    if player_num not in ['1', '2']:
         print "You are Player #1 or Player #2, please enter a 1 or 2 when asked your player number. "
         new_value(list_x_o)
     player_num = int(player_num)
@@ -42,13 +48,13 @@ def new_value(list_x_o):
     place = list_x_o[row][column]
 
     if place == "":
-        mark(list_x_o,row,column,player_num)
+        mark(list_x_o, row, column, player_num)
     else:
         print "That space is already taken. Please pick another space for your mark. "
         new_value(list_x_o)
 
 
-def mark(list_x_o,row,column,player_num):
+def mark(list_x_o, row, column, player_num):
     """This function puts the mark on the tic tac toe board"""
     if player_num == 1:
         list_x_o[row][column] = "X"
@@ -58,7 +64,7 @@ def mark(list_x_o,row,column,player_num):
     winner_check(list_x_o,player_num)  
 
 
-def winner_check(list_x_o,player_num):
+def winner_check(list_x_o, player_num):
     """ This function checks to see if there is a winner or the  board is full"""
     print "checking for winner"
     if player_num == 1:
@@ -84,12 +90,12 @@ def winner_check(list_x_o,player_num):
     elif list_x_o[0][2] == mark and list_x_o[1][1] == mark and list_x_o[2][0] == mark:
         condition = True
     print condition
-    if condition == True:
+    if condition is True:
         print "YOU WON!!!!!"
-        gameboard(list_x_o,True)
+        gameboard(list_x_o, True)
     elif "" not in list_x_o[0] and "" not in list_x_o[1] and "" not in list_x_o[2]:
         print "No winners here"
-        gameboard(list_x_o,False)
+        gameboard(list_x_o, False)
     else:
         gameboard(list_x_o)
 
@@ -110,6 +116,4 @@ if __name__ == "__main__":
     start()
 
 
-         
-
-         
+       
